5b5c8989edfb82f9b6b3e1004f917e80e6a75c21
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-08-19 04:26:56 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace Backseating_Begone
+{
+    [BepInPlugin("com.meds.backseatingbegone", "Backseating Begone", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> medsEmotesCards { get; private set; }
+        public static ConfigEntry<bool> medsEmotesTargetEnemies { get; private set; }
+        public static ConfigEntry<bool> medsEmotesTargetAllies { get; private set; }
+
+        private readonly Harmony harmony = new("com.meds.backseatingbegone");
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Backseating begone! Go on, git!");
+            medsEmotesCards = Config.Bind(new ConfigDefinition("Disable Emotes", "Cards"), true, new ConfigDescription("Disable emotes on cards."));
+            medsEmotesTargetEnemies = Config.Bind(new ConfigDefinition("Disable Emotes", "Target Enemies"), true, new ConfigDescription("Disable emotes that target enemies."));
+            medsEmotesTargetAllies = Config.Bind(new ConfigDefinition("Disable Emotes", "Target Allies"), true, new ConfigDescription("Disable emotes that target allies."));
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch]
+    public class Byebye
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MatchManager), "DoEmoteCard")]
+        public static bool DoEmoteCardPrefix()
+        {
+            if (Plugin.medsEmotesCards.Value)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Backseating Begone
+
+Disable emotes on cards, emotes that target enemies, and emotes that target allies.
+
+## Installation
+
+Install [BepInEx](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+
+Download the latest release and put it in your BepInEx _plugins_ folder.
+
+## Support
+
+Ping @stiffmeds in the #modding channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
